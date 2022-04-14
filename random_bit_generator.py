6c2e05b65b8bd1da8251ee53543e5022b8f68d71
razat74 <razat74@gmail.com>
2022-04-14 14:46:20 -0400

    added packets

--- random_bit_generator.py
+++ random_bit_generator.py
@@ -7,18 +7,20 @@ from datetime import datetime
 resolution = 16384
 amp = 4
 bits = resolution/16
-freq = int(input("enter frequency (1:100Hz ; 2:1kHz ; 3:10kHz ; 4:100kHz ; 5:1MHz): "))
+freq = 10
+# freq = int(input("enter frequency (1:100Hz ; 2:1kHz ; 3:10kHz ; 4:100kHz ; 5:1MHz): "))
+#
+# if freq == 1:
+#     freq =
+# elif freq == 2:
+#     freq = 1
+# elif freq == 3:
+#     freq = 0.1
+# elif freq == 2:
+#     freq = 0.01
+# else:
+#     freq = 1
 
-if freq == 1:
-    freq = 10
-elif freq == 2:
-    freq = 1
-elif freq == 3:
-    freq = 0.1
-elif freq == 2:
-    freq = 0.01
-else:
-    freq = 1
 name = str(input("enter name of test set: "))
 os.mkdir(name)
 step = float(freq) / resolution
@@ -46,46 +48,30 @@ with open(name+"/" + currentTime + "-RandomBits-FunctionGen.csv", 'w', newline='
 
     bits = int(bits)
     # make header
-    bitArr = []
-    new_Arr=[0]*200
-    for i in range(0, 100):
-        rand_bit = 0
-        bitArr.append(rand_bit)
+    zero_length = 200
+    new_Arr=[0]*zero_length
+    for j in range(0, 4):
+        new_Arr+=[1,0,1,0,1,1]
+        for i in range(0, 200):
+            rand_bit = random.randint(0, 1)
+            new_Arr.append(rand_bit)
+    for i in new_Arr:
         for res in range(0, int(resolution / bits)):
-            writer.writerow([time, rand_bit * amp])
+            writer.writerow([time, i * amp])
             time += step
             col += 1
         base = base + r
         last_bit = rand_bit
 
-    for i in range(0, bits - 101):
-        rand_bit = random.randint(0, 1)
-        bitArr.append(rand_bit)
-
-        for res in range(0, int(resolution / bits)):
-            writer.writerow([time, rand_bit * amp])
-            time += step
-            col += 1
-        base = base + r
-        last_bit = rand_bit
-    for res in range(0, int(resolution / bits)):
-        writer.writerow([time, amp])
-        time += step
-        col += 1
-    base = base + r
-    last_bit = rand_bit
 
     while col <= resolution + 13:
-        writer.writerow([time, last_bit * amp])
+        print(col)
+        writer.writerow([time, 0])
         col += 1
         time += step
 
 f.close()
 
-for i in range(5):
-        new_Arr+=[1,1,1,0,1]
-        new_Arr+=[random.randint(0, 1) for x in range(195)]
-        #new_Arr+=['Hey']
 with open(name + "/RandomBits-RawBits.csv", 'w', newline='') as f:
     writer = csv.writer(f)
     for x in new_Arr:
